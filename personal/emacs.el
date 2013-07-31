ff5f6bd46f29e96eb6d303605dc1bb7879c06c3d
Matthias Krull <m.krull@uninets.eu>
2013-07-31 02:35:10 +0200

    better flycheck fringe indicators

--- personal/emacs.el
+++ personal/emacs.el
@@ -13,4 +13,23 @@
 
 ;; enable line numbers
 (global-linum-mode t)
-(setq linum-format "%4d \u2502")
+(setq linum-format "%3d\u2502")
+
+;; flymake indicators
+;; each integer represents a row of pixels
+;; the bits represent the pixel in a row
+;; 
+;; 0 0 0 0 0 0 0 0 0
+;; 0 0 0 0 0 0 0 0 0
+;; 0 0 0 0 0 0 0 0 0
+;; 1 1 1 1 1 1 1 1 1
+;; 1 1 1 1 1 1 1 1 1
+;; 0 0 0 0 0 0 0 0 0
+;; 0 0 0 0 0 0 0 0 0
+;; 0 0 0 0 0 0 0 0 0
+;;
+;; is a horizontal bar
+(define-fringe-bitmap 'flycheck-fringe-exclamation-mark
+  [0 0 0 255 255 0 0 0] nil nil 'center)
+(define-fringe-bitmap 'question-mark
+  [0 0 0 255 255 0 0 0] nil nil 'center)
