73e6591c6978d36516e7d665ed6c36ca1a675fb8
Matthias Krull <m.krull@uninets.eu>
2013-09-11 10:45:53 +0200

    added htmlize to auto installed packages

--- personal/auto-install.el
+++ personal/auto-install.el
@@ -1,3 +1,9 @@
 ;; additional packages to autoinstall by prelude
-(prelude-require-packages '(rainbow-delimiters color-theme-solarized auto-complete google-c-style))
+(prelude-require-packages '(
+							rainbow-delimiters
+							color-theme-solarized
+							auto-complete
+							google-c-style
+							htmlize
+							))
 
