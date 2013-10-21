1a6d6f1f4025dea01063efa4bdcdb54beea7dbf9
Matthias Krull <m.krull@uninets.eu>
2013-10-21 21:17:21 +0200

    added evil
    vim just is the better editor.. emacs the better everything else

--- personal/auto-install.el
+++ personal/auto-install.el
@@ -1,10 +1,10 @@
 ;; additional packages to autoinstall by prelude
 (prelude-require-packages '(
-							rainbow-delimiters
-							color-theme-solarized
-							auto-complete
-							google-c-style
-							htmlize
-                                                        workgroups2
-							))
-
+       rainbow-delimiters
+       color-theme-solarized
+       auto-complete
+       google-c-style
+       htmlize
+       workgroups2
+       evil
+       ))
--- personal/emacs.el
+++ personal/emacs.el
@@ -1,11 +1,24 @@
 ;; smaller custom config additional to prelude and main files
 
+;; Set Frame width/height
+(defun arrange-frame (w h x y)
+  "Set the width, height, and x/y position of the current frame"
+  (let ((frame (selected-frame)))
+    (delete-other-windows)
+    (set-frame-position frame x y)
+    (set-frame-size frame w h)))
+
 ;; solarized colors
 (load-theme 'solarized-dark t)
 
 ;; add marmalade
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list
+ 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
+;; remove doc checks on elisp files
+(eval-after-load
+    'flycheck '(setq flycheck-checkers
+                     (delq 'emacs-lisp-checkdoc flycheck-checkers)))
 ;; auto complete
 (require 'auto-complete-config)
 (ac-config-default)
@@ -18,13 +31,18 @@
 (global-linum-mode t)
 (setq linum-format "%3d\u2502")
 
+;; evil!
+(evil-mode 1)
+;; disable key chords - they dont play well with evil
+(key-chord-mode -1)
+
 ;; fringe
 '(fringe-mode (quote (2 . 8)) nil (fringe))
 
 ;; flymake indicators
 ;; each integer represents a row of pixels
 ;; the bits represent the pixel in a row
-;; 
+;;
 ;; 0 0 0 0 0 0 0 0 0
 ;; 0 0 0 0 0 0 0 0 0
 ;; 0 0 0 0 0 0 0 0 0
@@ -42,6 +60,8 @@
 
 ;; google C/C++ style
 (add-hook 'c-mode-common-hook 'google-set-c-style)
+
+(arrange-frame 165 55 5 5)
 
 (require 'workgroups2)
 ;; change workgroups2 settings here
