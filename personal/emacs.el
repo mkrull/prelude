0f9e16c47b9d1714303bc1b2eddfbddd12a0ed1c
Matthias Krull <m.krull@uninets.eu>
2013-10-21 22:15:51 +0200

    added status line coloring based on evil mode

--- personal/emacs.el
+++ personal/emacs.el
@@ -35,6 +35,18 @@
 (evil-mode 1)
 ;; disable key chords - they dont play well with evil
 (key-chord-mode -1)
+;; change mode-line color by evil state
+(lexical-let ((default-color (cons (face-background 'mode-line)
+                                   (face-foreground 'mode-line))))
+  (add-hook 'post-command-hook
+            (lambda ()
+              (let ((color (cond ((minibufferp) default-color)
+                                 ((evil-insert-state-p) '("#e80000" . "#ffffff"))
+                                 ((evil-emacs-state-p)  '("#444488" . "#ffffff"))
+                                 ((buffer-modified-p)   '("#006fa0" . "#ffffff"))
+                                 (t default-color))))
+                (set-face-background 'mode-line (car color))
+                (set-face-foreground 'mode-line (cdr color))))))
 
 ;; fringe
 '(fringe-mode (quote (2 . 8)) nil (fringe))
